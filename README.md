1bb3504fbb2af5815c0a4be4140f7ae59e1de045
Pushkar Patel <thepushkarp@gmail.com>
2020-06-28 18:06:00 +0530

    Update Lab 9

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Numerical-Techniques
-Repository Containing Solution Codes of Assignments given in MA202: Numetical Techniques
+
+Repository Containing Solution Codes of Assignments given in MA202: Numerical Techniques
+
+Labs:
+
+- Lab 9 - Regression
+  - [Assignment PDF](/lab9/MA202_lab9.pdf)
+  - [Solution code](/lab9/lab9.m)
+  - [Explanation PDF by TA](/lab9/MA202_lab9_slides.pdf)
--- lab9/lab9.m
+++ lab9/lab9.m
@@ -1,15 +1,37 @@
 %% Regression analysis
+
+% Linear Regression - Obtain a straight line that best fits the data
+
+% Least Squares method for Linear Regression - Suppose a line that fits the
+% data has an equation f(x) = a1*x + a0. This generate a point (x, p) for
+% every corresponding x in our data. This p is the predicted value of the
+% data and it differes from the actual value y by |y - p|. The least
+% squares method gives us a line which minimises the sum of squares of all
+% the errors over all points in our data to give a line that best fits our
+% data.
+
+% Polynomial regresison - Obtain a curve of a given degree 'n' that best
+% fits the data.
+
+% NOTE: There's a builtin method called lsqcurvefit that plots the best-fit
+% curve of a given degree but I haven't used it here. Read about it from
+% the docs.
+
+
 %% Initialising
 clc;
 clear;
 close all;
 
+% Points corresponding to the first question
 x1 = [-3 -2 -1 0 1 2 3];
 y1 = [0.2774 0.8958 1.5651 3.4565 3.0601 4.8568 3.8982];
 
+% Points corresponding to the second question
 x2 = [0.8 1.4 2.7 3.8 4.8 4.9];
 y2 = [0.69 1.00 2.00 2.39 2.34 2.83];
 
+% Degrees for polynomial regression
 poly_features = [1 3 5 7];
 len = length(poly_features);
 
@@ -46,9 +68,9 @@ disp(b);
 
 figure(3);
 
-[b, y] = linregbuiltin(x1,y1);
+[b, y] = linregpolyapp(x1,y1);
 plot(x1, y1, 'r*', x1, y, 'b-');
-title("Built-in Linear Regression for Question 1");
+title("Linear Regression with poly. approx. for Question 1");
 xlabel('x');
 ylabel('f(x)');
 snapnow;
@@ -88,9 +110,9 @@ disp(b);
 
 figure(6);
 
-[b, y] = linregbuiltin(x2,y2);
+[b, y] = linregpolyapp(x2,y2);
 plot(x2, y2, 'r*', x2, y, 'b-');
-title("Built-in Linear Regression for Question 2");
+title("Linear Regression with poly. approx. for Question 2");
 xlabel('x');
 ylabel('f(x)');
 snapnow;
@@ -98,6 +120,12 @@ disp ("theta for Question 2 = ");
 disp(b);
 
 %% Polynomial Regression
+
+% This is a n-degree polynomial approximation to get the best-fitting curve
+% of n degrees.
+
+% It returns values of a0, a1, ... , an, a(n+1) from the equation
+% y = a0 + a1*x + a2*x^2 ... a(n+1)*x^n
 
 function ypred = polyreg(x,y, feature)
 
@@ -111,6 +139,10 @@ function ypred = polyreg(x,y, feature)
 end
 
 %% Linear Regression
+
+% This is Linear Regression algorithm using Least Square Approximation.
+
+% It returns the values of y, a0 and a1 from the equation y = a1*x + a0.
 
 function [b, ypred] = linreg(x,y)
 
@@ -138,9 +170,16 @@ function [b, ypred] = linreg(x,y)
 
 end
 
-%% Built in Linear Regression
+%% Linear Regression using Polynomical Approximation of first degree
 
-function [b, ypred] = linregbuiltin(x,y)
+% This is a straightforward first degree polynomial approximation to get
+% the best-fit straight line.
+
+% It returns the values of y, a0 and a1 from the equation y = a1*x + a0.
+
+% This is same as LS method for Linear Regression.
+
+function [b, ypred] = linregpolyapp(x,y)
     X = [ones(length(x),1) transpose(x)];
     y = transpose(y);
     b = X\y;
